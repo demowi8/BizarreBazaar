cc86876f47ab4ca3ca472c37b9bc455ca27be963
Desmond Williams <demowi8@gmail.com>
2021-08-19 09:18:07 -0700

    Add project files.

---
+++ BizarreBazaar.Data/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizarreBazaar.Data
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(1,2000, ErrorMessage ="Choose Wisely")]
+        [Display(Name="Stock")]
+        public int AmountOf { get; set; }
+        
+        [Required]
+        [MaxLength(150, ErrorMessage ="Too many words")]
+        public string Description { get; set; }
+    }
+}
---
+++ BizarreBazaar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BizarreBazaar.Startup))]
+namespace BizarreBazaar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
